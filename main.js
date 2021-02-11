303be2e3aaa4df839d8652b5e1f04fecd8b5d3a9
nathou7170 <nath.vincent7@gmail.com>
2021-02-11 16:04:06 +0100

    chat node.js

--- main.js
+++ main.js
@@ -1,22 +1,22 @@
-const app = require("express")();
-const http = require("http").Server(app);
-const io = require("socket.io")(http);
+var express = require('express');
+var app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 
-app.get("/", function(req, res) {
-  res.sendFile(__dirname + "/index.html");
+app.get('/', function(req,res){
+    res.sendFile(__dirname + "/index.html");
 });
 
-io.on("connection", function(socket) {
-  console.log('user connected');
-  socket.on("chat", function(msg) {
-    io.emit("chat", msg);
-  });
+http.listen(3000, function(){
+    console.log("localhost:3000");
 });
 
-io.on("disconnected", function() {
-  console.log("user is disconnected !");
-});
-
-http.listen(3000, function() {
-  console.log("Listening on localhost:3000");
+io.on('connection', function(socket){
+    console.log('connection');
+    socket.on('chat', function(msg){
+        io.emit('chat', msg);
+    });
+    socket.on('disconnect', () =>{
+        console.log('disconnected');
+    });
 });
